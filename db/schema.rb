f00252887237ba501b740c3f4954963fd142e412
gzn <gzndamon@gmail.com>
2017-11-19 19:37:49 +0800

    implement the wage part

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119093205) do
+ActiveRecord::Schema.define(version: 20171119112343) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.string   "text"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
