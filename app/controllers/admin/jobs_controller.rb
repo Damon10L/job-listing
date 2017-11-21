82e97fa9eaf4a8fde03a9685c0da2e032d84a407
gzn <gzndamon@gmail.com>
2017-11-21 23:10:41 +0800

    step6 order by wage

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -45,6 +45,18 @@ class Admin::JobsController < ApplicationController
     end
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
+  end
+
   def destroy
     @job = Job.find(params[:id])
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,7 +10,14 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.published.order('wage_lower_bound DESC')
+    when  'by_upper_bound'
+      Job.published.order('wage_upper_bound DESC')
+    else
+      Job.published.recent
+    end
   end
 
   def new
@@ -28,18 +35,6 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
-  end
-
-  def publish
-    @job = Job.find(params[:id])
-    @job.publish
-    redirect_to :back
-  end
-
-  def publish
-    @job = Job.find(params[:id])
-    @job.hide
-    redirect_to :back
   end
 
   def update
